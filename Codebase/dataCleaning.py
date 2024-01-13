5685c74f4f9dc0226b65c179e7d5f541336eac6c
Asmi <amit0629@uni.sydney.edu.au>
2024-01-14 03:36:36 +1100

    Performing basic data cleaning

--- Codebase/dataCleaning.py
+++ Codebase/dataCleaning.py
@@ -6,15 +6,31 @@ The original data is in the form of two separate csv files, one for the purposes
 In order to create a more robust machine learning model, I have chosen to combine these files into one single csv and then perform data cleaning on the whole dataset. 
 '''
 
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# Read CSV files into dataframes
-fraudTrain = pd.read_csv('Dataset/fraudTrain.csv')
-print("Trainging Dataset Size:", fraudTrain.size)
-fraudTest = pd.read_csv('Dataset/fraudTest.csv')
-print("Test Dataset Size:", fraudTest.size)
+def combineData():
+    # Reading CSV files into dataframes
+    fraudTrain = pd.read_csv('Dataset/fraudTrain.csv')
+    print("Trainging Dataset Size:", len(fraudTrain))
+    fraudTest = pd.read_csv('Dataset/fraudTest.csv')
+    print("Test Dataset Size:", len(fraudTest))
 
-# Combine these two datasets into one and create a new output file
-combined_data = pd.concat([fraudTrain, fraudTest], ignore_index=True)
-print("Final Dataset Size:", combined_data.size)
-combined_data.to_csv("Dataset/Fraud_Detection_Dataset.csv")
+    # Combining datasets into one to create final dataset 
+    combined_data = pd.concat([fraudTrain, fraudTest], ignore_index=True)
+    combined_data.to_csv("Dataset/Fraud_Detection_Dataset.csv")
+
+def readingDataset():
+    dataset = pd.read_csv('Dataset/Fraud_Detection_Dataset.csv')
+    return dataset
+
+# Data cleaning
+def dataClean(dataset):
+    dataset.info()
+    dataset = dataset.dropna()
+    dataset = dataset.drop(dataset.columns[0:2],axis=1) #not relevant
+    dataset.info()
+
+dataset = readingDataset()
+dataClean(dataset)
